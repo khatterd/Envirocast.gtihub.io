5c4ecb10ec4b997a45a8797ecd8fa3269abc54d9
khatterd <deveshkhatter@gmail.com>
2023-02-27 02:09:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 var city = 'brampton'
-    $.ajax({
-        method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/weather?city=' + city,
-        headers: { 'X-Api-Key': 'i3GlPtu1RgHdbWp5A5uo0A==pYXme5cs3LcU7U6t'},
-        contentType: 'application/json',
-        success: function(result) {
-            console.log(result);
-            var bout = document.getElementById('brampton');
-            bout.innerHTML = result.temp;
-        },
-        error: function ajaxError(jqXHR) {
-            console.error('Error: ', jqXHR.responseText);
-        }
-    });
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/weather?city=' + city,
+    headers: { 'X-Api-Key': 'i3GlPtu1RgHdbWp5A5uo0A==pYXme5cs3LcU7U6t'},
+    contentType: 'application/json',
+    xhrFields: {
+      withCredentials: true
+    },
+    success: function(result) {
+        console.log(result);
+        var bout = document.getElementById('brampton');
+        bout.innerHTML = result.temp;
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
+
 
 
     var city = 'chandigarh'

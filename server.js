4109b79d38690842fa19e959bbde7057fc02c888
Devesh Khatter <DeveshKhatter@gmail.com>
2023-02-27 23:06:46 -0500

    commit 69

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 const express = require('express');
-const app = express();
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const cors = require('cors');
 
+const app = express();
 
-
-app.use(cors());
-
-const apiProxy = createProxyMiddleware('/v1/weather', {
-    target: 'https://api.api-ninjas.com',
-    changeOrigin: true,
-    headers: {
-        'X-Api-Key': 'i3GlPtu1RgHdbWp5A5uo0A==pYXme5cs3LcU7U6t'
-    }
-});
-
-app.use('/v1/weather', apiProxy);
+app.use('/api/v1/weather', createProxyMiddleware({
+  target: 'https://api.api-ninjas.com',
+  changeOrigin: true,
+  headers: {
+    'X-Api-Key': process.env.API_KEY
+  }
+}));
 
 app.listen(3000, () => {
-    console.log(`Proxy server listening on port 3000`);
+  console.log('Proxy server listening on port 3000');
 });
